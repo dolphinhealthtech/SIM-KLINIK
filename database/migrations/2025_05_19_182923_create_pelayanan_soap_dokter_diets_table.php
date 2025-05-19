c5141e77d51e081720d7bb086079d7dd11a6ec33
Syclonce <dhrigos@gmail.com>
2025-05-19 20:02:59 +0700

    soap post

---
+++ database/migrations/2025_05_19_182923_create_pelayanan_soap_dokter_diets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelayanan_soap_dokter_diets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomor_rm');
+            $table->string('nama');
+            $table->string('no_rawat');
+            $table->string('sex');
+            $table->string('penjamin');
+            $table->date('tanggal_lahir');
+            $table->string('Jenis_diet');
+            $table->string('jenis_diet_makanan');
+            $table->string('jenis_diet_makanan_tidak');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelayanan_soap_dokter_diets');
+    }
+};
