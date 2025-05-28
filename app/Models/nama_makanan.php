229e803c7284d74b4bde0cd55df6d5eecf898784
Syclonce <dhrigos@gmail.com>
2025-05-28 20:29:40 +0700

    fix manana diet

--- app/Models/nama_makanan.php
+++ app/Models/nama_makanan.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,10 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class nama_makanan extends Model
 {
     use HasFactory;
-    
-    // Ubah nama tabel sesuai dengan tabel yang ada di database
-    protected $table = 'nama_makanan'; // tanpa 's' di akhir
-    
+
+
     protected $fillable = ['nama'];
 }
 
