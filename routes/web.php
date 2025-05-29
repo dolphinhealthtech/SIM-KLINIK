3d3c329e2130fece92c7dd1d51df521cf7269cdd
Syclonce <dhrigos@gmail.com>
2025-05-29 09:27:28 +0700

    fix icd 10

--- routes/web.php
+++ routes/web.php
@@ -304,8 +304,9 @@ Route::prefix('pemeriksaan')->group(function () {
             Route::post('/nama-makanan/import', [DataMasterMedisController::class,'nama_makananimport'])->name('nama_makanan.import');
 
             Route::get('/icd10', [DataMasterMedisController::class,'icd10'])->name('icd10.get');
-            Route::get('/icd10/sync', [DataMasterMedisController::class,'icd10add'])->name('icd10.sync');
-            Route::post('/icd10/delete', [DataMasterMedisController::class,'icd10delete'])->name('icd10.singkron');
+            Route::get('/icd10/sync', [DataMasterMedisController::class,'icd10singkron'])->name('icd10.store');
+            Route::get('/icd10/add', [DataMasterMedisController::class,'icd10add'])->name('icd10.singkron');
+            Route::post('/icd10/delete', [DataMasterMedisController::class,'icd10delete'])->name('icd10.destroy');
             Route::get('/icd10/export', [DataMasterMedisController::class, 'icd10export'])->name('icd10.export');
             Route::post('/icd10/import', [DataMasterMedisController::class, 'icd10import'])->name('sarana.import');
 
