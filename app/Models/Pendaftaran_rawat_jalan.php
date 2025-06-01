4b1faa95ed7e34a41899d292a45f5a07d5634383
Syclonce <dhrigos@gmail.com>
2025-06-01 11:36:55 +0700

    fix model huruf

--- app/Models/Pendaftaran_rawat_jalan.php
+++ app/Models/Pendaftaran_rawat_jalan.php
@@ -29,12 +29,12 @@ class Pendaftaran_rawat_jalan extends Model
 
     public function poli()
     {
-        return $this->belongsTo(Poli::class, 'poli_id');
+        return $this->belongsTo(poli::class, 'poli_id');
     }
 
     public function dokter()
     {
-        return $this->belongsTo(Dokter::class, 'dokter_id');
+        return $this->belongsTo(dokter::class, 'dokter_id');
     }
     public function pasien()
     {
