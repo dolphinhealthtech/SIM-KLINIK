cb9a4c9e24cf1f43f19571d1264eb300469983e5
Syclonce <dhrigos@gmail.com>
2025-07-06 16:56:55 +0000

    clear conflict

--- main/resources/views/components/dashboard/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- /.content-wrapper -->
-<footer class="main-footer">
-    <strong>Copyright &copy; 2024 – <?php echo date("Y"); ?>  <a href="https://dolphinhealthtech.co.id">dolphinhealthtech</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b> 2.0
-    </div>
-</footer>
--- main/routes/user.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function () {
-    return view('pasien.index');
-})->middleware(['auth', 'verified'])->name('user');
