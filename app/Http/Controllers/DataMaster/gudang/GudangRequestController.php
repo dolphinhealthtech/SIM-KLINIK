6227e859a0a00c87347c874c58160ce206738f3d
id4t <idatismyname@gmail.com>
2025-07-07 16:30:21 +0700

    pecah master gudang

--- app/Http/Controllers/DataMaster/gudang/GudangRequestController.php
+++ app/Http/Controllers/DataMaster/gudang/GudangRequestController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\DataMaster\inventaris;
+namespace App\Http\Controllers\DataMaster\gudang;
 
 use App\Http\Controllers\Controller;
 use App\Models\external_database;
