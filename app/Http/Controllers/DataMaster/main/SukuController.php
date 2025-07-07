4d0cde0c98dde9fb2c97a831bd83e75affe76eb6
Syclonce <dhrigos@gmail.com>
2025-07-07 14:53:24 +0700

    pembagian data

---
+++ app/Http/Controllers/DataMaster/main/SukuController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\DataMaster\main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class SukuController extends Controller
+{
+    //
+}
